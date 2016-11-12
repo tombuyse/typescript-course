9db3f4ee971f8c9c05f946e1cf2c38d360f352e1
Tom Buyse <tom.buyse@c4j.be>
2016-11-12 11:38:38 +0100

    interfaces demo

---
+++ src/person.ts
@@ -0,0 +1,18 @@
+enum Gender {
+    M,
+    F
+}
+
+interface Person {
+    name: string;
+    age: number;
+    gender: Gender
+}
+
+function printPerson(person: Person) {
+    console.log(`Hello ${person.name}`);
+}
+
+//if we remove or change a type, the editor should complain about this and compiling will not work
+const person = {name: 'Jefke', age: 30, gender: Gender.M};
+printPerson(person);
